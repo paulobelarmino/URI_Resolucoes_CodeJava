9a331201b57450fbd6881866bd4b64d88d542916
paulobelarmino <pabelarmino@gmail.com>
2020-07-11 16:27:01 -0300

    Problema 1020 - em 11/07/2020

--- Probl_1020/src/Main.java
+++ Probl_1020/src/Main.java
@@ -4,10 +4,11 @@ public class Main {
    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         int idadeDias = leitor.nextInt();
-        int anos, meses, dias;
+        int anos, meses, dias, diasRemanecentes;
         anos = idadeDias/365;
-        meses = idadeDias/30 - anos*12;
-        dias = idadeDias- meses*30 - anos*365;
+        diasRemanecentes = idadeDias - anos*365;
+        meses = diasRemanecentes/30;
+        dias = diasRemanecentes- meses*30;
         System.out.printf("%d ano(s)%n%d mes(es)%n%d dia(s)%n",anos,meses,dias);
     }
     
